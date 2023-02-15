24208710ab9958eea2c36a6499487f633b52c47c
Nikolay <m.nikolay1990@gmail.com>
2023-02-15 17:41:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import { Chart } from 'chart.js';
+
+import { LineChart } from './widgets/chart/ui/LineChart';
+
+import './App.css';
+
+function App() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		// setInterval(() => {}, 50000);
+		let parsedData = [];
+		const start = Date.now();
+		Papa.RemoteChunkSize = 1000000;
+		Papa.parse('../server/data.csv', {
+			download: true,
+			chunk: (result) => {
+				parsedData = parsedData.concat(...[result.data]);
+				console.log(parsedData);
+			},
+			skipEmptyLines: true,
+			complete: () => {
+				const end = Date.now();
+				const time = end - start;
+				console.log(new Date(time).getMilliseconds());
+				setData(parsedData.slice(-100));
+			},
+		});
+
+		// return () => {
+		// 	second;
+		// };
+	}, []);
+	if (data.length !== 0) {
+		console.log(data);
+	}
+	if (data.length < 0) {
+		return null;
+	}
+	if (data.length >= 100) {
+		return (
+			<div className='App'>
+				{/* {data.map((item, i) => {
+				if (i > 100) {
+					return null;
+				}
+				return <span key={item[0]}>{item}</span>;
+			})} */}
+				<LineChart parsedData={data} />
+			</div>
+		);
+	}
+	return <div>Loading...</div>;
+}
+
+export default App;
---
+++ src/widgets/chart/ui/LineChart.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+	Chart as ChartJs,
+	LineElement,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js';
+
+import styles from '../model/LineChart.module.scss';
+
+ChartJs.register(
+	LineElement,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	Title,
+	Tooltip,
+	Legend
+);
+
+export const LineChart = ({ parsedData }) => {
+	const [dates, setDates] = useState([]);
+	const [values, setValues] = useState([]);
+
+	const transformData = () => {
+		const transformedData = parsedData.map((item) => {
+			const separatedData = item[0]
+				.replaceAll("'", '')
+				.replaceAll('[', '')
+				.replaceAll(']', '')
+				.split(',');
+			const date = separatedData[0];
+			// setDates((prevState) => [...prevState, transformedData[0]]);
+			const value = separatedData[1];
+			// console.log(JSON.parse(item));
+			// const date = item.split(',');
+			return { date, value };
+		});
+		const datesArr = transformedData.map((item) => item.date);
+		const valuesArr = transformedData.map((item) => item.value);
+		return { datesArr, valuesArr };
+	};
+
+	useEffect(() => {
+		const { datesArr } = transformData();
+		const { valuesArr } = transformData();
+		setDates(datesArr);
+		setValues(valuesArr);
+	}, []);
+	// console.log(dates);
+	// console.log(values);
+
+	const data = {
+		// labels: ['Mon', 'Tue', 'Wed'],
+		labels: dates,
+		datasets: [
+			{
+				label: 'Легенда индикатора №1',
+				// data: [0.15, 0.12, 0.23],
+				data: values,
+				backgroundColor: 'red',
+				borderColor: 'gold',
+				pointBorderColor: 'yellow',
+				fill: true,
+				tension: 0.1,
+			},
+		],
+	};
+
+	const options = {
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'Название таблицы',
+			},
+		},
+		scales: {
+			y: {
+				min: 0,
+				max: 0.2,
+			},
+		},
+	};
+
+	return (
+		<div className={styles.wrapper}>
+			<Line data={data} options={options}></Line>
+		</div>
+	);
+};
